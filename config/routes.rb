8afd6ab7e5cab3873cb10a3da7a2143ec383aea7
Akshay-Ranga <akshauranga9478@gmail.com>
2017-08-14 20:23:31 +0530

    "updated with menus & pages"

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   resources :routes
   resources :buses
   resources :logins
+  get "/:privacy" => "static#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
